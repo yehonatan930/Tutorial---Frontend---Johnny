a77035deffc7e1be4241cc19c15415a91c69d92a
johnny <324825470@idf.il>
2022-10-07 20:20:58 +0300

    get posts by user

--- src/api/UsersAPI.ts
+++ src/api/UsersAPI.ts
@@ -1,5 +1,6 @@
 import HttpClient from "./HttpClient";
 import User from "../models/User";
+import PostDTO from "../models/PostDTO";
 
 export default class UsersAPI extends HttpClient {
   private static classInstance?: UsersAPI;
@@ -18,13 +19,17 @@ export default class UsersAPI extends HttpClient {
 
   public getAllUsers = async () => await this.instance.get<User[]>("/");
 
-  public getUser = async (id: string) => await this.instance.get<User>(`${id}`);
+  public getUser = async (name: string) =>
+    await this.instance.get<User>(`${name}`);
+
+  public getUserPosts = async (name: string) =>
+    await this.instance.get<PostDTO[]>(`${name}/posts`);
 
   public getLoggedInUser = async () => {
     const user = await this.instance.get<User>(`currentLoggedIn`);
     return user;
   };
 
-  public deleteUser = async (id: string) =>
-    await this.instance.delete<User>(`${id}`);
+  public deleteUser = async (name: string) =>
+    await this.instance.delete<User>(`${name}`);
 }
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,13 +1,36 @@
-import { useContext } from "react";
+import "./Profile.css";
+import Avatar from "@mui/material/Avatar";
+import { useContext, useEffect, useState } from "react";
 import { LoggedInUserContext } from "../../contexts/LoggedInUserContext";
+import Typography from "@mui/material/Typography";
+import PostsList from "../../components/PostsList/PostsList";
+import User from "../../models/User";
+import PostDTO from "../../models/PostDTO";
+import UsersAPI from "../../api/UsersAPI";
 
 const Profile = () => {
   const loggedInUserContext = useContext(LoggedInUserContext);
   const user = loggedInUserContext.user;
 
+  const [userPosts, setUserPosts] = useState<PostDTO[]>([]);
+
+  useEffect(() => {
+    const getAllPostCardsOfUser = async (user: User) => {
+      const { data } = await UsersAPI.getInstance().getUserPosts(user.name);
+      setUserPosts(data);
+    };
+    getAllPostCardsOfUser(user!);
+  }, [user]);
+
   return (
     <>
-      <div>{user?.name}</div>
+      <div id="profile-header">
+        <Avatar sx={{ width: "6rem", height: "6rem" }} src={user?.avatarSrc} />
+        <Typography variant="h5" sx={{}}>
+          {user?.name}
+        </Typography>
+      </div>
+      <PostsList posts={userPosts}></PostsList>
     </>
   );
 };
