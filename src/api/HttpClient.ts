56f0abeb757928bad1b7a588e55b28f157a842cd
johnny <324825470@idf.il>
2022-10-18 17:46:39 +0300

    trying move to profile

--- src/api/HttpClient.ts
+++ src/api/HttpClient.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 export default abstract class HttpClient {
   protected readonly instance: AxiosInstance;
+  public controller: AbortController | null = new AbortController();
 
   public constructor(baseURL: string) {
     this.instance = axios.create({
